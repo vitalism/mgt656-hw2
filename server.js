ce5e9cc684c0e11aa54fd5e04dfec76d5be08995
David Yin <david.jy.yin@gmail.com>
2015-01-28 04:26:04 +0000

    Add nickname route

--- server.js
+++ server.js
@@ -2,7 +2,11 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(request, response){
-    response.send('Hello MGT-656!');
+    response.send('Hello MGT-656! I am a complete Ninja now!');
+});
+
+app.get('/nickname', function(request, response){
+    response.send('smiling sheep');
 });
 
 app.listen(process.env.PORT || 4000);
